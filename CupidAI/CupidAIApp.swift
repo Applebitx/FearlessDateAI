8f7e8d7c0f3905f49a7aafe33329da279540aab3
Applebitx <applebitx@gmail.com>
2023-05-13 21:49:37 +0300

    Initial Commit

---
+++ CupidAI/CupidAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupidAIApp.swift
+//  CupidAI
+//
+//  Created by Денис Сторожик on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupidAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
